ba949f1470a8badf6b33bcff911cd0f64a99d2a6
phucbm <hiphucbui@gmail.com>
2022-10-25 16:29:44 +0700

    :construction: split test modules

---
+++ dev/js/test-init.js
@@ -0,0 +1,20 @@
+import html from "../html/init.html";
+
+export function testInit(root){
+    root.insertAdjacentHTML('beforeend', html);
+
+    // Init: default attribute
+    EasySelect.init();
+
+    // Init: jQuery plugin
+    $('#init-jquery').easySelect();
+
+    // Init: vanilla JS: DOM element
+    EasySelect.init(document.querySelector('#init-vanilla-dom'));
+
+    // Init: vanilla JS: jQuery element
+    EasySelect.init($('#init-vanilla-jquery'));
+
+    // Init: vanilla JS: CSS selector string
+    EasySelect.init('#init-vanilla-string');
+}
--- dev/script.js
+++ dev/script.js
@@ -8,6 +8,10 @@ import './easy-select.css';
 // source script
 import '@/_index';
 
+// test script
+import {testInit} from "./js/test-init";
+import {testLayout} from "./js/test-layout";
+
 // import package info
 const packageInfo = require('../package.json');
 
@@ -19,17 +23,11 @@ const title = `${packageInfo.prettyName} v${packageInfo.version}`;
 document.title = `[DEV] ${title} - ${packageInfo.description}`;
 document.querySelector('[data-title]').innerHTML = title;
 
+// add HTML
+const root = document.querySelector('#root');
+
 /**
- * Lib test
+ * Test script
  */
-// Init: jQuery plugin
-$('#init-jquery').easySelect();
-
-// Init: vanilla JS: DOM element
-EasySelect.init(document.querySelector('#init-vanilla-dom'));
-
-// Init: vanilla JS: jQuery element
-EasySelect.init($('#init-vanilla-jquery'));
-
-// Init: vanilla JS: CSS selector string
-EasySelect.init('#init-vanilla-string');
+testInit(root);
+testLayout(root);
