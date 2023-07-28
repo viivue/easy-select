7a7459c2041e78106dcc033848e2968baa96beee
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2023-07-28 13:55:47 +0700

    feat: #22 rebuild add function

--- src/_index.js
+++ src/_index.js
@@ -353,24 +353,46 @@ class EasySelect{
      * @returns {boolean|EasySelect}
      */
     add(value){
-        if(this.isDisabled) return false;
+        if(this.isDisabled) return false
 
         // avoid duplicate value
-        if(this.selectTagData.filter(option => option.value === value).length > 0){
-            if(this.config.warning) console.warn(`[ES] ${value} will not be added due to duplicating`);
-            return false;
+        const isDuplicate = (value) => {
+            if(this.selectTagData.filter(option => option.value === value).length > 0){
+                if(this.config.warning) console.warn(`[ES] ${value} will not be added due to duplicating`)
+                return true
+            }
         }
 
         // add new option to select tag
-        this.selectTag.insertAdjacentHTML('beforeend', `<option value="${value}">${value}</option>`);
+        const addNewOption = (value, label) => {
+            if(typeof value === 'object'){
+                return this.selectTag.insertAdjacentHTML('beforeend', `<option value='${value}'>${label}</option>`)
+            }else if(typeof value === 'string'){
+                return this.selectTag.insertAdjacentHTML('beforeend', `<option value='${value}'>${value}</option>`)
+            }
+        }
+
+
+        if(typeof value === 'object'){
+            const { value, label } = value
+            if(isDuplicate(value)){
+                return false
+            }
+            addNewOption(value, label)
+        }else if(typeof value === 'string'){
+            if(isDuplicate(value)){
+                return false
+            }
+            addNewOption(value)
+        }
 
         // refresh
-        this.refresh();
+        this.refresh()
 
         // Event: on add
-        fireEvent(this, 'onAdded', {newValue: value});
+        fireEvent(this, 'onAdded', { newValue: value })
 
-        return this;
+        return this
     }
 }
 
