7e7897978cb4111c4b46eabd8c9e0c8a3b77dbc4
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2023-08-03 11:59:17 +0700

    fix: update demo

--- dev/js/test-search.js
+++ dev/js/test-search.js
@@ -2,14 +2,4 @@ import html from "../html/search.html";
 
 export function testSearch(root){
     root.insertAdjacentHTML('beforeend', html);
-
-    // Init: default attribute
-    EasySelect.init('#test-search', {
-        id: 'test-search',
-        search: true,
-    });
-
-    // keep dropdown open
-    const search = EasySelect.get('test-search');
-    search.open();
 }
