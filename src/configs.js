713dedc96bcb819c1ec034301f516df686134afa
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2023-08-03 15:43:28 +0700

    refactor: reformat code

--- src/configs.js
+++ src/configs.js
@@ -4,45 +4,45 @@ import {uniqueId} from "./utils";
  * Classes
  * */
 export const CLASSES = {
-  wrapper: 'easy-select',
-  dropdownOpen: 'es-dropdown-open',
-  current: 'es-current',
-  dropdown: 'es-dropdown',
-  option: 'es-option',
-  active: 'es-active',
-  disabled: 'es-disabled',
-  nativeSelect: 'es-native',
-  enabled: 'es-enabled',
-  ignore: 'es-ignore',
-  searchInput: 'es-search-input',
-  searchEnabled: 'es-search-enabled',
-  searchWrapper: 'es-search-wrapper',
-  searchEmpty: 'es-search-empty',
+    wrapper: 'easy-select',
+    dropdownOpen: 'es-dropdown-open',
+    current: 'es-current',
+    dropdown: 'es-dropdown',
+    option: 'es-option',
+    active: 'es-active',
+    disabled: 'es-disabled',
+    nativeSelect: 'es-native',
+    enabled: 'es-enabled',
+    ignore: 'es-ignore',
+    searchInput: 'es-search-input',
+    searchEnabled: 'es-search-enabled',
+    searchWrapper: 'es-search-wrapper',
+    searchEmpty: 'es-search-empty',
 }
 /**
  * Attributes
  * */
 export const ATTRS = {
-  init: 'data-easy-select',
-  wrapperID: 'data-es-id',
-  optionAttr: 'data-es-option',
-  value: 'data-es-value',
+    init: 'data-easy-select',
+    wrapperID: 'data-es-id',
+    optionAttr: 'data-es-option',
+    value: 'data-es-value',
 }
 /**
  * Defaults
  * */
 export const DEFAULTS = {
-  id: uniqueId('es-'),
-  nativeSelect: false,
-  warning: true,
-  wrapDefaultSelect: true,
-  closeOnChange: true,
-  align: "left",
+    id: uniqueId('es-'),
+    nativeSelect: false,
+    warning: true,
+    wrapDefaultSelect: true,
+    closeOnChange: true,
+    align: "left",
 
-  // show search input inside dropdown
-  search: false,
-  emptySearchText: "There are no options", // optional, text appear when search empty
-  searchPlaceHolder: "Type to search...",
-  customDropDownOptionHTML: option => {
-  },
+    // show search input inside dropdown
+    search: false,
+    emptySearchText: "There are no options", // optional, text appear when search empty
+    searchPlaceHolder: "Type to search...",
+    customDropDownOptionHTML: option => {
+    },
 };
